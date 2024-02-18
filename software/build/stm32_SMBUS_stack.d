9cf8a85ffbc7d5bec585e965e025be1b6bc3a0ee
Daniel <dadamkovic@gmail.com>
2024-02-18 13:06:20 -0500

    Restructured repo

---
+++ software/build/stm32_SMBUS_stack.d
@@ -0,0 +1,69 @@
+build/stm32_SMBUS_stack.o: \
+ Middlewares/ST/STM32_SMBus_Stack/src/stm32_SMBUS_stack.c \
+ Middlewares/ST/STM32_SMBus_Stack/inc/stm32_SMBUS_stack.h \
+ SMBus_PMBus_Stack/Target/stm32_config_stack.h Core/Inc/main.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ Core/Inc/stm32f0xx_hal_conf.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h \
+ Drivers/CMSIS/Include/core_cm0.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_smbus.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h \
+ Middlewares/ST/STM32_SMBus_Stack/inc/stm32_PMBUS_stack.h \
+ Middlewares/ST/STM32_SMBus_Stack/inc/stm32_SMBUS_stack.h
+Middlewares/ST/STM32_SMBus_Stack/inc/stm32_SMBUS_stack.h:
+SMBus_PMBus_Stack/Target/stm32_config_stack.h:
+Core/Inc/main.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+Core/Inc/stm32f0xx_hal_conf.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h:
+Drivers/CMSIS/Include/core_cm0.h:
+Drivers/CMSIS/Include/cmsis_version.h:
+Drivers/CMSIS/Include/cmsis_compiler.h:
+Drivers/CMSIS/Include/cmsis_gcc.h:
+Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_smbus.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
+Middlewares/ST/STM32_SMBus_Stack/inc/stm32_PMBUS_stack.h:
+Middlewares/ST/STM32_SMBus_Stack/inc/stm32_SMBUS_stack.h:
